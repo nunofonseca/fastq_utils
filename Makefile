90a6913a091cc573f064ebb4d582a39dbf934f3e
Nuno Fonseca <firstname.lastname@gmail.com>
2017-06-21 09:46:55 +0100

    Wrapper to fastq_info to support BAM and bzip2 compressed files

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=0.9.0
+VERSION=0.9.2
 # Requires zlib libraries
 
 
@@ -6,10 +6,11 @@ all:
 	make -C src
 
 install:
-	mkdir -p bin && make -C src install
+	mkdir -p bin && make -C src install && cp sh/*.sh bin
 
 release:
 	sed -i "s/#define VERSION.*/#define VERSION \"$(VERSION)\"/" src/*.c
+	sed -i "s/#define VERSION.*/#define VERSION \"$(VERSION)\"/" src/*.h
 
 tests: FORCE
 	./run_tests.sh
---
+++ sh/fastq_validator.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# =========================================================
+# Copyright 2012-2017,  Nuno A. Fonseca (nuno dot fonseca at gmail dot com)
+#
+#
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# if not, see <http://www.gnu.org/licenses/>.
+#
+#
+# =========================================================
+# wrapper to fastq_info to support BAM and bzip2
+
+FILES=$*
+
+function file_extension {
+    echo $*|sed -E "s/([^ \.]+)\.//g" 
+}
+set -e
+# check extension
+ext=`file_extension $1`
+if [ "$ext-" == "bam-" ]; then
+    #hmm, this now validates bams...kind of
+    # samtools version should be 1 or above
+    f=$1    
+    echo "BAM file"
+    # check if the BAM contains unaligned reads
+    echo "Checking for unmapped reads"
+    UN=`samtools view -c -f 4 $f`
+    if [ "$UN-" == "0-" ]; then
+	echo "ERROR: No unaligned reads found in $f." > /dev/stderr
+	exit 1
+    fi
+    named_pipe=.`basename .$f`.pipe.fastq
+    mkfifo $named_pipe
+    echo "Converting BAM to fastq"
+    samtools bam2fq $f > $named_pipe &
+    
+    FILES2PROCESS=$named_pipe
+    FILES2DELETE=$named_pipe
+else
+    FILES2PROCESS=
+    FILES2DELETE=
+    for f in $FILES; do
+	ext=`file_extension $f`
+	if [ "-$ext" == "-bz2" ] || [ "-$ext" == "-bzip2" ] ; then
+	    echo BZIP file
+	    named_pipe=.`basename .$f`.pipe.fastq
+	    mkfifo $named_pipe
+	    bunzip2 -k  -c $f > $named_pipe  &
+	    FILES2PROCESS="$FILES2PROCESS $named_pipe"
+	    FILES2DELETE="$FILES2DELETE $named_pipe"
+	else
+	    FILES2PROCESS="$FILES2PROCESS $f"
+	fi
+    done
+fi
+fastq_info $FILES2PROCESS
+
+if [ "-$FILES2DELETE" != "-" ]; then
+    #echo -n "Removing named pipes..."
+    rm -f $FILES2DELETE
+    #echo "done."
+fi
+exit 0
