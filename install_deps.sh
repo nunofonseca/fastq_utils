9b59b687b09d6c3de3fa2da10aa0d013c780b9e5
Nuno Fonseca <firstname.lastname@gmail.com>
2017-07-03 09:34:50 +0100

    Install new dependency: samtools 0.1.x

--- install_deps.sh
+++ install_deps.sh
@@ -9,4 +9,11 @@ pushd zlib-1.2.11
 make
 make install
 popd
-
+#############################################
+#
+wget https://sourceforge.net/projects/samtools/files/samtools/0.1.19/samtools-0.1.19.tar.bz2/download -O samtools-0.1.19.tar.bz2
+tar -jxvf samtools-0.1.19.tar.bz2 
+pushd samtools-0.1.19
+make
+make razip
+popd
