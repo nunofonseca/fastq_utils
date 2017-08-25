99550419893269df749b3ad062c0e86c6883c0e2
Nuno Fonseca <firstname.lastname@gmail.com>
2017-08-26 00:12:18 +0100

    Also install samtools 1.4

--- install_deps.sh
+++ install_deps.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
+TOPLEVEL_DIR=$PWD
+mkdir -p deps
+
 ############################################
 ## zlib
-wget http://zlib.net/zlib-1.2.11.tar.gz
+wget -c http://zlib.net/zlib-1.2.11.tar.gz
 tar xvzf zlib-1.2.11.tar.gz
 pushd zlib-1.2.11
 ./configure --prefix `pwd`/..
@@ -11,9 +14,23 @@ make install
 popd
 #############################################
 #
-wget https://sourceforge.net/projects/samtools/files/samtools/0.1.19/samtools-0.1.19.tar.bz2/download -O samtools-0.1.19.tar.bz2
-tar -jxvf samtools-0.1.19.tar.bz2 
+if [ ! -e deps/samtools-0.1.19.tar.bz2  ]; then
+    wget -c https://sourceforge.net/projects/samtools/files/samtools/0.1.19/samtools-0.1.19.tar.bz2/download -O deps/samtools-0.1.19.tar.bz2
+fi
+tar jxvf deps/samtools-0.1.19.tar.bz2 
 pushd samtools-0.1.19
 make
 make razip
 popd
+
+###############################################
+# Downloads were failing often so keep a local copy to speedup the installation
+if [ ! -e deps/samtools-1.4.1.tar.bz2 ] ; then
+    wget -c https://sourceforge.net/projects/samtools/files/samtools/1.4.1/samtools-1.4.1.tar.bz2/download -O deps/samtools-1.4.1.tar.bz2
+fi
+
+tar xvjf deps/samtools-1.4.1.tar.bz2
+pushd samtools-1.4.1
+#./configure  prefix=$TOPLEVEL_DIR
+make -j $J prefix=$TOPLEVEL_DIR
+make install prefix:=$TOPLEVEL_DIR
