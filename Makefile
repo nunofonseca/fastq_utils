cb9762f4615d1b2d525a9aad310469d469d30809
Nuno Fonseca <firstname.lastname@gmail.com>
2017-08-26 00:18:03 +0100

    New version 0.11.0

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-VERSION=0.10.2
+VERSION=0.11.0
 # Requires zlib and samtools 0.1.9
 all:
 	make -C src
--- src/fastq.h
+++ src/fastq.h
@@ -20,7 +20,7 @@
 #
 # =========================================================
 */
-#define VERSION "0.10.2"
+#define VERSION "0.11.0"
 
 #define DEFAULT  0
 #define CASAVA18 1
