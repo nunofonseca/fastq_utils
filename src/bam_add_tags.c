66a17aa5421888f5f4c2a0e4a61836263d43b3ed
Nuno Fonseca <firstname.lastname@gmail.com>
2017-08-29 12:42:48 +0100

    Use named parameters and add option to extract the trancript id from the sequence name

--- src/bam_add_tags.c
+++ src/bam_add_tags.c
@@ -23,12 +23,18 @@
 #include <bam.h>
 #include <sam.h>
 #include <kstring.h>      
+#include <getopt.h>
 
 #include <string.h>
 #include <assert.h>
 
 #include "fastq.h"
 
+#define MAX_FEAT_LEN 50
+typedef struct trans_gene_map {
+  char tx[MAX_FEAT_LEN];
+  char gx[MAX_FEAT_LEN];
+} TGM;
 /*
  * Returns 1 on sucess, 0 otherwise.
  */
@@ -87,33 +93,144 @@ short get_barcodes(const char *s,char *sample,char *umi,char *cell,
   return(1);
 }
 
-int main(int argc, char *argv[])  
-{  
+void print_usage(int error) {
+  char msg[]="Usage: bam_add_tags --inbam <in.bam> --outbam <out.bam or - for stdout> [--tx] [--tx2gx map_file_gene_2_trans.tsv]";
+  if (error>0 ) {
+    PRINT_ERROR(msg); exit(error);
+  } else {
+    fprintf(stderr,"%s\n",msg);
+  }
+}
+
+static ulong char2ulong(char *str) {
+
+  ulong hash = 0;
+  int c;
+  while ((c = *str++))
+    hash = c + (hash << 6) + (hash << 16) - hash;  
+  return(hash);
+}
+
+
+char* get_gene( char* trans,hashtable ht) {
+
+  // lookup
+  ulong key=char2ulong(trans);
+  TGM* e=(TGM*)get_object(ht,key);
+  while (e!=NULL) {     
+    if ( !strcmp(trans,e->tx) ) return(e->gx);
+    e=(TGM*)get_next_object(ht,key);
+  }
+  return(NULL);
+}
+
+
+int main(int argc, char *argv[])   {  
   short out2stdout=0;
   bamFile in; 
   bamFile out; 
+  // inbam
+  // outbam
+  // --tx_to_gx
+  // --tx field (seq)
+  char *inbam_file=NULL;
+  char *outbam_file=NULL;
+  char *map_file=NULL;
+  hashtable t2g_ht=NULL;
+  
+  static int verbose=0;  
+  static int help=FALSE;
+  static int tx_tag;
+  static struct option long_options[] = {
+    {"verbose", no_argument,       &verbose, TRUE},
+    {"tx", no_argument,      &tx_tag, TRUE},
+    {"help",   no_argument, &help, TRUE},
+    {"inbam",  required_argument, 0, 'i'},
+    {"outbam",  required_argument, 0, 'o'},
+    {"tx_2_gx",  required_argument, 0, 'm'},
+    {0,0,0,0}
+  };
 
-  if (argc != 3) {  
-    PRINT_ERROR("Usage: bam_add_tags <in.bam> <out.bam or - for stdout>");  
-    return(PARAMS_ERROR_EXIT_STATUS);  
-  }  
-  // Open file and exit if error
-  //in = bam_open(argv[1], "rb");
-  in = strcmp(argv[1], "-")? bam_open(argv[1], "rb") : bam_dopen(fileno(stdin), "rb"); 
-  out2stdout = strcmp(argv[2], "-")? 0 : 1; 
-  out = strcmp(argv[2], "-")? bam_open(argv[2], "w") : bam_dopen(fileno(stdout), "w"); 
+
+  // process arguments
+  while (1) {
+    /* getopt_long stores the option index here. */
+    int option_index = 0;
+    
+    int c = getopt_long (argc, argv, "i:o:m:",
+			 long_options, &option_index);      
+    if (c == -1) // no more options
+      break;
+    
+    switch (c) {      
+    case 'i': inbam_file=optarg; break;
+    case 'o': outbam_file=optarg; break;
+    case 'm': map_file=optarg; break;
+    default: break;
+    }
+  }
+  if ( inbam_file == NULL ) print_usage(1);
+  if ( outbam_file == NULL ) print_usage(1);
+  if ( !tx_tag && map_file != NULL ) {
+    PRINT_ERROR("missing  --tx when --tx_2_gx is provided\n");
+    print_usage(PARAMS_ERROR_EXIT_STATUS);
+  }
+  if (help ) {
+    print_usage(0);
+    exit(0);
+  }
+
+  
+  in = strcmp(inbam_file, "-")? bam_open(inbam_file, "rb") : bam_dopen(fileno(stdin), "rb"); 
+  out2stdout = strcmp(outbam_file, "-")? 0 : 1; 
+  out = strcmp(outbam_file, "-")? bam_open(outbam_file, "w") : bam_dopen(fileno(stdout), "w"); 
   if (in == 0 ) {  
-    PRINT_ERROR("Failed to open BAM file %s", argv[1]);  
+    PRINT_ERROR("Failed to open BAM file %s", inbam_file);  
     return(PARAMS_ERROR_EXIT_STATUS);  
   }  
 
   if (out == 0) {  
-    PRINT_ERROR("Failed to open BAM file %s", argv[2]);  
+    PRINT_ERROR("Failed to open BAM file %s", outbam_file);  
     return(PARAMS_ERROR_EXIT_STATUS);  
-  }  
-
-  unsigned long num_alns=0;
+  }
   
+  unsigned long num_map_entries=0;   
+  if ( map_file!=NULL ) {
+    FILE *map_fd;
+    if ((map_fd=fopen(map_file,"r"))==NULL) {
+      PRINT_ERROR("Failed to open file %s", map_file);  
+      exit(1);
+    }
+    // map transcript to gene 
+    t2g_ht=new_hashtable(100001);
+    char buf[1000];
+
+    while (!feof(map_fd) ) {
+      char *s=fgets(&buf[0],1000,map_fd);
+      if (s==NULL || s[0]=='\0') continue;
+      
+      TGM *e=(TGM*)malloc(sizeof(TGM));
+      if ( e==NULL ) {
+       PRINT_ERROR("Failed to allocate memory\n");
+       exit(1);
+      }
+      // gene trans
+      char *gx=strtok(s,"\t\n");
+      char *tx=strtok(NULL,"\t\n");
+      if (gx==NULL || tx==NULL ) {
+	PRINT_ERROR("Failed to find the gene and transcript ids in %s\n",s);
+	exit(1);
+      }
+      strcpy(e->tx,tx);
+      strcpy(e->gx,gx);
+      insere(t2g_ht,char2ulong(e->tx),e);// create a dummy entry (key is unique)
+      ++num_map_entries;
+    }
+    fclose(map_fd);
+    fprintf(stderr,"unique gene/transcript pairs %lu\n",t2g_ht->n_entries);
+  }
+
+  unsigned long num_alns=0;  
   // ***********
   // Copy header
   bam_header_t *header;
@@ -125,7 +242,7 @@ int main(int argc, char *argv[])
 
   if (!out2stdout) {
     fprintf(stderr,"bam_add_tags version %s\n",VERSION);
-    fprintf(stderr,"Processing %s\n",argv[1]);
+    fprintf(stderr,"Processing %s\n",inbam_file);
   }
   //
   // place holders to keep the information
@@ -142,21 +259,39 @@ int main(int argc, char *argv[])
     char *qn=bam1_qname(aln);
     //fprintf(stderr,"-->%s\n",qn);
     if (get_barcodes(qn,&sample[0],&umi[0],&cell[0],&sample_len,&umi_len,&cell_len)) {
-      //fprintf(stderr,"YES-->%s %s %s\n",cell,sample,umi);
-      // 
+
       if ( umi_len > 0 )   // UMI
-	bam_aux_append(aln, "UM", 'Z', umi_len+1, umi);
+	bam_aux_append(aln, "UM", 'Z', umi_len+1, (uint8_t *)umi);
       if ( cell_len > 0 )  // cellular barcode sequence as reported by the sequencer
-	bam_aux_append(aln, "CR", 'Z', cell_len+1, cell); 
+	bam_aux_append(aln, "CR", 'Z', cell_len+1, (uint8_t *)cell); 
       if ( sample_len > 0 ) // sample index
-	bam_aux_append(aln, "BC", 'Z', sample_len+1, sample);
-      //fprintf(stderr,"YES-->%s %s %s\n",cell,sample,umi);
+	bam_aux_append(aln, "BC", 'Z', sample_len+1, (uint8_t *)sample);
+      if (tx_tag ) {
+	char *tx=NULL;
+	if ( aln->core.tid >= 0 ) {
+	  tx=header->target_name[aln->core.tid];
+	  //fprintf(stderr,">>>>TX: %s\n",tx);
+	  bam_aux_append(aln,"TX",'Z',strlen(tx)+1,(uint8_t *)tx);
+	  //
+	  if ( map_file != NULL ) {
+	    char* gene=(char*)get_gene(tx,t2g_ht);
+	    if ( gene!=NULL ) {
+	      bam_aux_append(aln,"GX",'Z',strlen(gene)+1,(uint8_t *)gene);
+	    } // else { exit(3); }  should this fail now?
+	  }
+	}
+      }
     }
     bam_write1(out,aln);
   }
- end_loop:
+
   //bam_close(in); 
   bam_close(out);
-  bam_destroy1(aln);  
+  bam_destroy1(aln);
+  if (!out2stdout) {
+    fprintf(stderr,"Processing %s complete\n",inbam_file);
+  }
+
   return(0);
 }
+
