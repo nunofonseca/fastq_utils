6954ca9d5e5867f17b4838055bd330bd28dc206f
Nuno Fonseca <firstname.lastname@gmail.com>
2017-09-19 17:33:23 +0100

    More tests

--- run_tests.sh
+++ run_tests.sh
@@ -109,7 +109,8 @@ must_fail ./src/fastq_info -f tests/test_dot.fastq.gz
 ##
 ## just checks the exit status
 must_succeed ./src/fastq_info tests/test_dot.fastq.gz 
-must_succeed 	./src/fastq_info tests/test_1.fastq.gz 
+must_succeed 	./src/fastq_info tests/test_1.fastq.gz
+must_succeed 	./src/fastq_info tests/test_30_1.fastq.gz  tests/test_30_2.fastq.gz 
 must_succeed 	./src/fastq_info tests/test_2.fastq.gz 
 must_succeed 	./src/fastq_info tests/test_13.fastq.gz
 must_succeed 	./src/fastq_info tests/test_17.fastq.gz
