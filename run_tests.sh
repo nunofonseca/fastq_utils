1858d26b5cac417ab22637c91b67d82a8ea1d9eb
Nuno Fonseca <firstname.lastname@gmail.com>
2017-09-19 14:50:25 +0100

    Relax Casava check
    Former-commit-id: f95f3349fe9e7b04d12417aa5d0769e6efb80d01

--- run_tests.sh
+++ run_tests.sh
@@ -123,6 +123,8 @@ must_succeed 	time -p ./src/fastq_info tests/pe_bug14.fastq.gz tests/pe_bug14.fa
 #must_succeed 	time -p ./src/fastq_info tests/c18_1M_1.fastq.gz tests/c18_1M_1.fastq.gz 
 must_succeed 	time -p ./src/fastq_info tests/casava.1.8i.fastq.gz pe 
 must_succeed 	time -p ./src/fastq_info tests/solexa_1.fastq.gz tests/solexa_2.fastq.gz
+must_succeed 	time -p ./src/fastq_info  tests/casava.1.8_readname_trunc_1.fastq.gz tests/casava.1.8_readname_trunc_2.fastq.gz
+must_succeed 	time -p ./src/fastq_info  tests/casava.1.8_readname_trunc_1.fastq.gz tests/casava.1.8_2.fastq.gz
 
 must_fail "./src/fastq_info --help"
 ##
