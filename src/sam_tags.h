a32aef4c941667c07115f1f188cac7ef13a64e8e
Nuno Fonseca <firstname.lastname@gmail.com>
2017-10-06 17:05:34 +0100

    SAM/BAM tags used
    Former-commit-id: c06c34d8242e2016a49b7d855424ebf4d39227b7

---
+++ src/sam_tags.h
@@ -0,0 +1,41 @@
+/*
+# =========================================================
+# Copyright 2017,  Nuno A. Fonseca (nuno dot fonseca at gmail dot com)
+#
+# This file is part of fastq_utils.
+#
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# if not, see <http://www.gnu.org/licenses/>.
+#
+#
+# =========================================================
+*/
+// TAGS used in unaligned SAM/BAM files
+#define RNAME "*"
+#define POS "0"
+#define CIGAR "*"
+#define RNEXT "*"
+#define PNEXT "0"
+#define MAPQ "255" 
+
+#define CELL_TAG "QR" 
+#define CELL_QUAL_TAG "QC" 
+
+#define UMI_TAG "QX" 
+#define UMI_QUAL_TAG "OQ" 
+
+#define SAMPLE_TAG "BC"
+#define SAMPLE_QUAL_TAG  "QT"
+
+#define ORIG_RN_TAG "ON"
+#define ORIG_QUAL_TAG "OP" 
