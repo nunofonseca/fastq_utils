3b28708f33a3451888731223f7562a7d06505c5c
Nuno Fonseca <firstname.lastname@gmail.com>
2018-01-17 00:16:52 +0000

    Upgrade samtools to 1.6

--- install_deps.sh
+++ install_deps.sh
@@ -26,12 +26,12 @@ popd
 ###############################################
 # Downloads were failing often so keep a local copy to speedup the installation
 echo "Installing samtools 1.x" 
-if [ ! -e deps/samtools-1.5.tar.bz2 ] ; then
-    wget -c https://sourceforge.net/projects/samtools/files/samtools/1.5/samtools-1.5.tar.bz2/download -O deps/samtools-1.5.tar.bz2
+if [ ! -e deps/samtools-1.6.tar.bz2 ] ; then
+    wget -c https://sourceforge.net/projects/samtools/files/samtools/1.6/samtools-1.6.tar.bz2/download -O deps/samtools-1.6.tar.bz2
 fi
 
-tar xvjf deps/samtools-1.5.tar.bz2
-pushd samtools-1.5
+tar xvjf deps/samtools-1.6.tar.bz2
+pushd samtools-1.6
 ./configure  prefix=$TOPLEVEL_DIR
 make -j $J
 #prefix=$TOPLEVEL_DIR
