4a7082f3cbf5d577284048266c318c56dfcca187
Rolando Fernandez <rolando@ebi.ac.uk>
2018-05-03 12:01:51 +0100

    parsing stderr from the fastq validator

--- wrapper/validator/validator.py
+++ wrapper/validator/validator.py
@@ -7,10 +7,13 @@ class Validator:
     def validate(self, file_path):
         report = ValidationReport()
 
-        process = subprocess.Popen(["fastq_info", file_path], stdout=subprocess.PIPE)
+        process = subprocess.Popen(["fastq_info", file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         process.wait()
-        for line in process.stderr:
-            report.log_error(line)
+        err_lines = process.stderr.readlines()
+        for line in err_lines:
+            line_str = line.decode("utf-8")
+            if "ERROR" in line_str:
+                report.log_error(line_str.rstrip())
 
         report.state = "INVALID" if report.errors else "VALID"
 
