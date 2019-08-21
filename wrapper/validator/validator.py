7f0345de9bcde8a97790b944661651c142eba212
prabh-t <prabhat.totoo@gmail.com>
2019-08-21 13:35:51 +0100

    Handle any exception that may occur using fastq validation to make sure a valid string format is returned

--- wrapper/validator/validator.py
+++ wrapper/validator/validator.py
@@ -7,12 +7,15 @@ class Validator:
     def validate(self, file_path):
         report = ValidationReport()
 
-        process = subprocess.Popen(["fastq_info", "-r", "-s", file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = process.communicate()
-        err_lines = stderr.decode().split('\n')
-        for line in err_lines:
-            if "ERROR" in line:
-                report.log_error(line.rstrip())
+        try:
+            process = subprocess.Popen(["fastq_info", "-r", "-s", file_path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            stdout, stderr = process.communicate()
+            err_lines = stderr.decode().split('\n')
+            for line in err_lines:
+                if "ERROR" in line:
+                    report.log_error(line.rstrip())
+        except (TypeError, ValueError, Exception) as e:
+            report.log_error(e)
 
         report.state = "INVALID" if report.errors else "VALID"
 
